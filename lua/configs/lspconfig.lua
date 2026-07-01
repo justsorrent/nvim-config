8b9964b48e9a262ebb1e264214c98a869500f010
Luca Sorrentino <sorrentinoluca.1999@gmail.com>
2026-07-01 21:26:35 +0200

    feat: new lsp for obsidian markdown

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua" }
+local servers = { "html", "cssls", "lua", "markdown_oxide" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
